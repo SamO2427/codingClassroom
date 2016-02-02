891f463d5727543bd5e93cbc497d8dd5a59fe10f
charliechoy <charliechoycc@gmail.com>
2016-02-02 18:48:53 +0800

    ch2added
    ch2added

---
+++ ch 2-6.php
@@ -0,0 +1,300 @@
+<!--<?php
+echo "Hello World!";
+?>  -->
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<style>
+			body {
+				background-image: url("125.png");
+				background-repeat: no-repeat;
+				background-size: cover;
+			}
+
+			#notes {
+				border-radius: 25px;
+				padding-left: 3%;
+				padding-right: 3%;
+				width: 30%;
+				height: 580px;
+				border: 2px solid black;
+				text-align: left;
+				position: absolute;
+				top: 3%;
+				left: 15%;
+				font-size: 30px;
+				background-color: lightblue;
+			}
+			#choices {
+				width: 10%;
+				height: 10%;
+				position: absolute;
+				text-align: center;
+				top: 80%;
+				left: 88%;
+			}
+
+			.option {
+				border-radius: 25px;
+				width: 100%;
+				border: 1px solid black;
+				position: relative;
+				font-size: 25px;
+				background-color: yellow;
+				color: black;
+				cursor: pointer;
+				float: left;
+				margin: 5%;
+			}
+			#question {
+				border-radius: 25px;
+				padding-left: 3%;
+				padding-right: 3%;
+				width: 25%;
+				height: 430px;
+				border: 2px solid black;
+				text-align: left;
+				position: absolute;
+				top: 15%;
+				left: 55%;
+				font-size: 30px;
+				background-color: lightgreen;
+			}
+
+			#btn1 {
+				border-radius: 25px;
+				width: 35%;
+				border: 3px solid black;
+				position: relative;
+				font-size: 25px;
+				background-color: #FF11FF;
+				float: left;
+				cursor: pointer;
+				margin: 4%
+			}
+
+			#btn2 {
+				border-radius: 25px;
+				width: 55%;
+				border: 3px solid black;
+				position: relative;
+				font-size: 25px;
+				background-color: #FFFF55;
+				float: left;
+				cursor: pointer;
+				margin: 4%
+			}
+
+			#btn3 {
+				border-radius: 25px;
+				width: 50%;
+				border: 3px solid black;
+				position: relative;
+				font-size: 25px;
+				background-color: #11FFFF;
+				float: left;
+				cursor: pointer;
+				margin: 4%
+			}
+
+			#dbox {
+				display: none;
+				position: fixed;
+				top: 0px;
+				left: 0px;
+				width: 100%;
+				z-index: 10;
+			}
+			#dialog {
+				display: none;
+				border-radius: 16px;
+				background-color: #00FF00;
+				width: 550px;
+				position: fixed;
+				z-index: 10;
+			}
+
+			#dbody {
+				text-align: center;
+				font-size: 30px;
+				padding: 20px;
+			}
+
+			#dfooter {
+				text-align: right;
+				padding: 10px;
+			}
+
+			#next {
+				display: none;
+				border-radius: 25px;
+				width: 10%;
+				top: 200px;
+				left: 83%;
+				height: 50px;
+				border: 1px solid black;
+				position: absolute;
+				font-size: 25px;
+				background-color: white;
+				color: black;
+				float: left;
+				cursor: pointer;
+				margin: 5%;
+			}
+		</style>
+		<script>
+			function CustomAlert1() {
+				this.render = function(words) {
+					var winW = window.innerWidth;
+					var winH = window.innerHeight;
+					var dialog = document.getElementById('dialog');
+					dialog.style.left = (winW / 2) - (550 * .5) + "px";
+					dialog.style.top = (winH * 0.2) + "px";
+					dialog.style.display = "block";
+					dialog.style.background = "#00FF00";
+					var dbody = document.getElementById('dbody');
+					dbody.style.height = (winH / 4) + "px";
+					var dbox = document.getElementById('dbox');
+					dbox.style.height = winH + "px";
+					dbox.style.display = "block";
+					document.getElementById('dbody').innerHTML = words;
+					document.getElementById('dfooter').innerHTML = '<button onclick="Alert1.ok()">Very Good!</button>';
+				}
+				this.ok = function() {
+					document.getElementById('dialog').style.display = "none";
+					document.getElementById('dbox').style.display = "none";
+					document.getElementById('btn1').disabled = "true";
+					document.getElementById('btn2').disabled = "true";
+					document.getElementById('btn3').disabled = "true";
+					document.getElementById('next').style.display = "block";
+				}
+			}
+
+			var Alert1 = new CustomAlert1();
+
+			function CustomAlert2() {
+				this.render = function(words) {
+					var winW = window.innerWidth;
+					var winH = window.innerHeight;
+					var dialog = document.getElementById('dialog');
+					dialog.style.left = (winW / 2) - (550 * .5) + "px";
+					dialog.style.top = (winH * 0.2) + "px";
+					dialog.style.display = "block";
+					dialog.style.background = "#FF0000";
+					var dbody = document.getElementById('dbody');
+					dbody.style.height = (winH / 4) + "px";
+					var dbox = document.getElementById('dbox');
+					dbox.style.height = winH + "px";
+					dbox.style.display = "block";
+					document.getElementById('dbody').innerHTML = words;
+					document.getElementById('dfooter').innerHTML = '<button onclick="Alert2.ok()">Try Again</button>';
+				}
+				this.ok = function() {
+					document.getElementById('dialog').style.display = "none";
+					document.getElementById('dbox').style.display = "none";
+					document.getElementById('btn1').disabled = "true";
+				}
+			}
+
+			var Alert2 = new CustomAlert2();
+
+			function CustomAlert3() {
+				this.render = function(words) {
+					var winW = window.innerWidth;
+					var winH = window.innerHeight;
+					var dialog = document.getElementById('dialog');
+					dialog.style.left = (winW / 2) - (550 * .5) + "px";
+					dialog.style.top = (winH * 0.2) + "px";
+					dialog.style.display = "block";
+					dialog.style.background = "#FF0000";
+					var dbody = document.getElementById('dbody');
+					dbody.style.height = (winH / 4) + "px";
+					var dbox = document.getElementById('dbox');
+					dbox.style.height = winH + "px";
+					dbox.style.display = "block";
+					document.getElementById('dbody').innerHTML = words;
+					document.getElementById('dfooter').innerHTML = '<button onclick="Alert3.ok()">Try Again</button>';
+				}
+				this.ok = function() {
+					document.getElementById('dialog').style.display = "none";
+					document.getElementById('dbox').style.display = "none";
+					document.getElementById('btn2').disabled = "true";
+				}
+			}
+
+			var Alert3 = new CustomAlert3();
+		</script>
+		<title>Coding Classroom for Kids</title>
+
+		<!--background music -->
+		<audio id="background_audio" autoplay="autoplay" loop="loop" >
+			<source src="Design/bgm/village10.ogg" type="audio/ogg" />
+			<source src="Design/bgm/village10.mp3" type="audio/mpeg" />
+		</audio>
+	</head>
+
+	<body>
+		<div id="notes">
+			<p>
+				There is another loop: do-while-loop
+			</p>
+			<p>
+				int candy = 0;
+			</p>
+			<p>
+				do
+			</p>
+			<p>
+				{candy=candy+1}
+			</p>
+			<p>
+				while(candy<3)
+			</p>
+			<p>do-while-loop will do at least 1 loop, which is not the same as while-loop and for-loop</p>
+		</div>
+
+		<div id="question">
+			<h3 style="text-align: center">Little Quiz</h3>
+			<p>
+				Which of the following is NOT a type of loop?
+			</p>
+			<button id="btn1" onclick="Alert2.render('It is not correct. Try another one')">
+				for-loop
+			</button>
+			<button id="btn2" onclick="Alert3.render('It is not correct. Try another one')">
+				do-while-loop
+			</button>
+			<button id="btn3" onclick="Alert1.render('Well Done! You are correct! You can move on to exercise part of chapter 2')">
+				do-for-loop
+			</button>
+		</div>
+		<div id="dbox">
+			<div id="dialog">
+				<div id="dbody"></div>
+				<div id="dfooter"></div>
+			</div>
+		</div>
+
+		<a href="./ex2.1.php">
+		<button id="next">
+			Next ->
+		</button></a>
+
+		<div id="choices">
+			<a href="./chapter2.php">
+			<button class="option">
+				Back
+			</button></a>
+			<br>
+			<a href="./usermenu.php">
+			<button class="option">
+				Menu
+			</button></a>
+		</div>
+	</body>
+</html>
---
+++ ex2.1.php
@@ -0,0 +1,277 @@
+<!--<?php
+echo "Hello World!";
+?>  -->
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<style>
+			body {
+				background-image: url("125.png"
+				);
+				background-repeat: no-repeat;
+				background-size: cover;
+			}
+
+			a {
+				text-decoration: none;
+			}
+
+			#menu {
+				border-radius: 25px;
+				width: 40%;
+				height: 520px;
+				border: 2px solid black;
+				text-align: center;
+				position: absolute;
+				top: 10%;
+				left: 30%;
+				font-size: 250%;
+				background-color: lightblue;
+			}
+
+			#menu1 {
+				font-size: 20px;
+				text-align: left;
+				position: relative;
+				left: 10%;
+			}
+			.options {
+				border-radius: 5px;
+				width: 70%;
+				height: 5%;
+				border: 1px solid black;
+				text-align: left;
+				position: relative;
+				top: 0%;
+				cursor: pointer;
+				background-color: yellow;
+				color: black;
+			}
+
+			#choices {
+				width: 10%;
+				height: 10%;
+				position: absolute;
+				text-align: center;
+				top: 80%;
+				left: 88%;
+			}
+
+			.option {
+				border-radius: 25px;
+				width: 100%;
+				border: 1px solid black;
+				position: relative;
+				font-size: 25px;
+				background-color: yellow;
+				color: black;
+				cursor: pointer;
+				float: left;
+				margin: 5%;
+			}
+			#dbox {
+				display: none;
+				position: fixed;
+				top: 0px;
+				left: 0px;
+				width: 100%;
+				z-index: 10;
+			}
+			#dialog {
+				display: none;
+				border-radius: 16px;
+				background-color: #00FF00;
+				width: 550px;
+				position: fixed;
+				z-index: 10;
+			}
+
+			#dbody {
+				text-align: center;
+				font-size: 30px;
+				padding: 20px;
+			}
+
+			#dfooter {
+				text-align: right;
+				padding: 10px;
+			}
+
+			#next {
+				display: none;
+				border-radius: 25px;
+				width: 10%;
+				top: 200px;
+				left: 83%;
+				height: 50px;
+				border: 1px solid black;
+				position: absolute;
+				font-size: 25px;
+				background-color: white;
+				color: black;
+				float: left;
+				cursor: pointer;
+				margin: 5%;
+			}
+		</style>
+
+		<script>
+			function allowDrop(ev) {
+				ev.preventDefault();
+			}
+
+			function drag(ev) {
+				ev.dataTransfer.setData("src", ev.target.id);
+			}
+
+			function drop(ev) {
+				ev.preventDefault();
+				var src = document.getElementById(ev.dataTransfer.getData("src"));
+				var srcParent = src.parentNode;
+				var tgt = ev.currentTarget.firstElementChild;
+
+				ev.currentTarget.replaceChild(src, tgt);
+				srcParent.appendChild(tgt);
+			}
+		</script>
+
+		<script>
+			function CustomAlert1() {
+				this.render = function(words) {
+					var winW = window.innerWidth;
+					var winH = window.innerHeight;
+					var dialog = document.getElementById('dialog');
+					dialog.style.left = (winW / 2) - (550 * .5) + "px";
+					dialog.style.top = (winH * 0.2) + "px";
+					dialog.style.display = "block";
+					dialog.style.background = "#00FF00";
+					var dbody = document.getElementById('dbody');
+					dbody.style.height = (winH / 4) + "px";
+					var dbox = document.getElementById('dbox');
+					dbox.style.height = winH + "px";
+					dbox.style.display = "block";
+					document.getElementById('dbody').innerHTML = words;
+					document.getElementById('dfooter').innerHTML = '<button onclick="Alert.ok()">Very Good!</button>';
+				}
+				this.ok = function() {
+					document.getElementById('dialog').style.display = "none";
+					document.getElementById('dbox').style.display = "none";
+					document.getElementById('next').style.display = "block";
+				}
+			}
+
+			var Alert1 = new CustomAlert1();
+
+			function CustomAlert2() {
+				this.render = function(words) {
+					var winW = window.innerWidth;
+					var winH = window.innerHeight;
+					var dialog = document.getElementById('dialog');
+					dialog.style.left = (winW / 2) - (550 * .5) + "px";
+					dialog.style.top = (winH * 0.2) + "px";
+					dialog.style.display = "block";
+					dialog.style.background = "#FF0000";
+					var dbody = document.getElementById('dbody');
+					dbody.style.height = (winH / 4) + "px";
+					var dbox = document.getElementById('dbox');
+					dbox.style.height = winH + "px";
+					dbox.style.display = "block";
+					document.getElementById('dbody').innerHTML = words;
+					document.getElementById('dfooter').innerHTML = '<button onclick="Alert.ok()">Try Again</button>';
+				}
+				this.ok = function() {
+					document.getElementById('dialog').style.display = "none";
+					document.getElementById('dbox').style.display = "none";
+				}
+			}
+
+			var Alert2 = new CustomAlert2();
+			var Alert = Alert2;
+
+			function correct() {
+				var a = document.getElementById("b2");
+				var c = document.getElementById("a1").contains(a);
+				var b = document.getElementById("b3");
+				var d = document.getElementById("a2").contains(b);
+				if (c == true && d == true) {
+					Alert1.render('Well Done!<br> You are correct!<br>You can move on to next question');
+					document.getElementById('next').style.display = "block";
+				} else {
+					Alert2.render('It is not correct. Try Again');
+				}
+			}
+		</script>
+		<title>Coding Classroom for Kids</title>
+
+	</head>
+
+	<body>
+
+		<div id="menu">
+			<p>
+				Q2.1
+			</p>
+			<p style="font-size: 30px">
+				Move the lines to correct place
+			</p>
+			<div id="menu1">
+				<div id = "a1" ondrop="drop(event)" ondragover="allowDrop(event)">
+					<div class="options" id="b1" draggable="true" ondragstart="drag(event)">
+						else
+					</div>
+				</div>
+				<p>
+					{int c = 0;}
+				</p>
+				<div id = "a2" ondrop="drop(event)" ondragover="allowDrop(event)">
+					<div class="options" id="b2" draggable="true" ondragstart="drag(event)">
+						if(b == true)
+					</div>
+				</div>
+				<p>
+					{int c = 1;}
+				</p>
+				<div id = "a3" ondrop="drop(event)" ondragover="allowDrop(event)">
+					<div class="options" id="b3" draggable="true" ondragstart="drag(event)">
+						else if(a == true)
+					</div>
+				</div>
+				<p>
+					{int c = 2;}
+				</p>
+			</div>
+			<button onclick="correct()">
+				Submit
+			</button>
+		</div>
+
+		<div id="dbox">
+			<div id="dialog">
+				<div id="dbody"></div>
+				<div id="dfooter"></div>
+			</div>
+		</div>
+
+		<div id="choices">
+			<a href="./ex3.php">
+			<button class="option">
+				Back
+			</button></a>
+			<br>
+			<a href="./usermenu.php">
+			<button class="option">
+				Menu
+			</button></a>
+		</div>
+
+		<a href="./ex2.2.php">
+		<button id="next">
+			Next ->
+		</button></a>
+	</body>
+</html>
